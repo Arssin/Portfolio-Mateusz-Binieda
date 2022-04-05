bc6e045b18b73cd13da2c626c75a7787008cd65b
Arssin <mateusz.binieda@onet.pl>
2022-04-05 12:41:29 +0200

    add animation to text

--- src/Components/HomeBody/HomeBody.js
+++ src/Components/HomeBody/HomeBody.js
@@ -9,6 +9,15 @@ export function HomeBody() {
             <h2 className={styles.welcomeText}>Welcome!</h2>
             <h2 className={styles.welcomeText2}>Welcome!</h2>
           </div>
+          <h2 className={styles.takeSeatText}>
+            <span className={styles.h2Text}>Take</span>
+            <span className={styles.h2Text}>a</span>
+            <span className={styles.h2Text}>seat</span>
+            <span className={styles.h2Text}>and</span>
+            <span className={styles.h2Text}> enjoy</span>
+            <span className={styles.h2Text}>my</span>
+            <span className={styles.h2Text}>portfolio...</span>
+          </h2>
         </div>
       </section>
       <section>About Me</section>
