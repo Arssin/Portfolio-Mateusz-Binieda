6c25249364ec65cf9b1cccca535c79aad6deae06
Arssin <mateusz.binieda@onet.pl>
2022-04-11 12:42:17 +0200

    Refactor code, add image, change welcome page

--- src/Components/HomeBody/HomeBody.js
+++ src/Components/HomeBody/HomeBody.js
@@ -5,6 +5,10 @@ import { Start } from '../HomeBody/Start/Start';
 export function HomeBody() {
   return (
     <div className={styles.body}>
+      <div className={styles.backgroundText}>
+        <h2 className={styles.welcomeText}>Welcome!</h2>
+        <h2 className={styles.welcomeText2}>Welcome!</h2>
+      </div>
       <Start id="home" />
       <Aboutme id="about" />
       <section id="my projects">My Projects</section>
