0f84abc9bf85653e28f7ec2ee9882e6b07c73a83
Arssin <mateusz.binieda@onet.pl>
2022-04-28 13:52:17 +0200

    Refactor contact me

--- src/Components/HomeBody/Contact/ContactForm.js
+++ src/Components/HomeBody/Contact/ContactForm.js
@@ -1,5 +1,41 @@
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 import styles from './ContactForm.module.scss';
 
 export function ContactForm() {
-  return <form className={styles.form}>Cotact</form>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'Portfolio-MB', form.current, '072amLaSZWMrYWE3v').then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      },
+    );
+    e.target.reset();
+  };
+
+  return (
+    <form ref={form} className={styles.form} onSubmit={sendEmail}>
+      <div className={styles.div}>
+        <input placeholder="Name" className={styles.input} type="text" name="name" />
+      </div>
+      <div className={styles.div}>
+        <input placeholder="Email" className={styles.input} type="email" name="email" />
+      </div>
+      <div className={styles.div}>
+        <input placeholder="Subject" className={styles.input} type="text" name="subject" />
+      </div>
+      <div className={styles.div}>
+        <textarea className={styles.message} placeholder="Message" name="message" />
+      </div>
+      <div>
+        <input type="submit" value="Send" />
+      </div>
+    </form>
+  );
 }
