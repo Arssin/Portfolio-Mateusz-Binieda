a6f99c33287cab15a69fdd853d47093b7265370e
Arssin <mateusz.binieda@onet.pl>
2022-05-18 00:36:27 +0200

    feature: add styles for closing hamburger, add animations for that

--- src/Components/Navbar/Hamburger.js
+++ src/Components/Navbar/Hamburger.js
@@ -3,7 +3,7 @@ import styles from '../Navbar/Hamburger.module.scss';
 export function Hamburger({ isOpen, onClick }) {
   return (
     <div className={styles.toggle} onClick={onClick} aria-label={isOpen ? 'Otwórz menu' : 'Zamknij menu'}>
-      {isOpen ? <div className={styles.bar} /> : 'X'}
+      {isOpen ? <div className={styles.bar} /> : <div className={styles.xbar} />}
     </div>
   );
 }
