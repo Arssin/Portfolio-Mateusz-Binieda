173ac16b1a0bc0698b19f8e9e39dea693f944893
Arssin <mateusz.binieda@onet.pl>
2022-06-20 20:33:53 +0200

    feature: add pictures for projects

--- src/Components/HomeBody/MyProjects/MyProjects.js
+++ src/Components/HomeBody/MyProjects/MyProjects.js
@@ -88,6 +88,40 @@ export const MyProjects = () => {
           </a>
         </div>
       </div>
+      <div data-aos="fade-up" className={styles.boxDiv}>
+        <div className={styles.box}>
+          <a className={styles.link} href="https://star-wars-quizz.netlify.app/" rel="noreferrer" target="_blank">
+            <img className={styles.image} src={starwars} alt="Picture star wars quizz project" />
+          </a>
+          <span className={styles.span}> Star Wars Quizz App</span>
+          <a href="https://github.com/Arssin/Star-Wars-Quizz-App-CodersCamp-2021-2022" rel="noreferrer" target="_blank">
+            <BsGithub className={styles.icons} alt="Clickable Github icon Star Wars Quizz App" />
+          </a>
+        </div>
+        <div className={styles.box}>
+          <a className={styles.link} href="https://michal-team-projekt-2.netlify.app" rel="noreferrer" target="_blank">
+            <img className={styles.image} src={bking} alt="Picture of Bking project" />
+          </a>
+          <span className={styles.span}> Bking - Your Vacation Rentals!</span>
+          <a href="https://github.com/Arssin/Bking-App-CodersCamp-2021-2022" rel="noreferrer" target="_blank">
+            <BsGithub className={styles.icons} alt="Clickable Github icon Bking" />
+          </a>
+        </div>
+        <div className={styles.box}>
+          <a
+            className={styles.link}
+            href="https://snake-game-practice-mateusz-binieda.vercel.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img className={styles.image} src={snake} alt="Picture of snake project" />
+          </a>
+          <span className={styles.span}> Very Simple Snake Game (PC)</span>
+          <a href="https://github.com/Arssin/SnakeGame-Practice-MateuszBinieda" rel="noreferrer" target="_blank">
+            <BsGithub className={styles.icons} alt="Clickable Github icon, snake game" />
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
